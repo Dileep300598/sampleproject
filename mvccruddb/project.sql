7d1d1dc87ecc2ac21e74036c9b2011d89ffa5c49
Dileep300598 <89853695+Dileep300598@users.noreply.github.com>
2021-09-01 11:38:37 +0530

    Add files via upload

---
+++ mvccruddb/project.sql
@@ -0,0 +1,7 @@
+Create Table project(
+id int primary key identity(1,1),
+Name nvarchar,
+StartDate date,
+EndDate date,
+Budget Decimal
+)
---
+++ mvccruddb/role.sql
@@ -0,0 +1,8 @@
+
+
+
+
+Create Table Role(
+Role int primary key identity(1,2),
+name varchar(max) not null 
+)
